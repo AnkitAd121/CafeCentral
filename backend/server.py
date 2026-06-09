4bafac5aee6c5c4c239c3da527c88777b21247d4
AnkitAd121 <pridechhetri@gmail.com>
2026-06-09 11:43:16 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -295,7 +295,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["https://cafe-central-tawny.vercel.app", "http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
